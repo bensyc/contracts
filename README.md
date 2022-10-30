e982b32fbc7bd0edbad3ef073b6c2b01c4192489
sshmatrix <cryptosauron@protonmail.com>
2022-10-30 08:56:55 +0100

    README

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 
 ## [Install Foundry](https://getfoundry.sh/)
 
+## Install dependency
+`forge install foundry-rs/forge-std`
+
 ## Rinkeby Testnet
  `./test/rinkeby.sh`
--- script/rinkeby.sh
+++ script/rinkeby.sh
@@ -1,1 +1,1 @@
-source .env && forge script ./script/Rinkeby.s.sol --rpc-url $RINKEBY_RPC_URL  --private-key $PRIVATE_KEY --broadcast --verify --etherscan-api-key $ETHERSCAN_KEY -vvvv RUST_BACKTRACE=full
+source .env && forge script ./script/Rinkeby.s.sol --rpc-url $RINKEBY_RPC_URL  --private-key $RINKEBY_PRIVATE_KEY --broadcast --verify --etherscan-api-key $ETHERSCAN_KEY -vvvv RUST_BACKTRACE=full
